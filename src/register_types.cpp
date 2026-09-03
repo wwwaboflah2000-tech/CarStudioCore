fef482b841fdb2076477f55c365d6756385dba88
wwwaboflah2000-tech <www.aboflah2000@gmail.com>
2026-09-03 22:21:55 +0300

    Implement module initialization for CarModeler

---
+++ src/register_types.cpp
@@ -0,0 +1,25 @@
+#include <gdextension_interface.h>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+#include "car_modeler.h"
+
+using namespace godot;
+
+void initialize_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) return;
+    ClassDB::register_class<CarModeler>();
+}
+
+void uninitialize_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) return;
+}
+
+extern "C" {
+GDExtensionBool GDE_EXPORT car_studio_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+    godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+    init_obj.register_initializer(initialize_module);
+    init_obj.register_terminator(uninitialize_module);
+    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+    return init_obj.init();
+}
+}
